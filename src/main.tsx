8f8947dfeebb44d9fc7b303659866709fa4083f1
C0L2 <vacara7samue7@gmail.com>
2024-04-08 09:57:52 +0300

    - initialized multiLang component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+import { I18nextProvider } from 'react-i18next'
+import i18next from 'i18next'
+import global_en from './locales/en/global.json'
+import global_ro from './locales/ro/global.json'
+import global_ru from './locales/ru/global.json'
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'auto',
+  fallbackLng: 'en',
+  resources: {
+    en: {
+      global: global_en,
+    },
+    ro: {
+      global: global_ro,
+    },
+    ru: {
+      global: global_ru,
+    },
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>,
 )
